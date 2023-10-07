c87762050b3ea04c11a615f672e24e178f5fd6e9
boool369 <127376060+boool369@users.noreply.github.com>
2023-10-07 17:41:38 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,26 +1,58 @@
 import os
+import argparse
+import sys
+from datetime import datetime
 
+def rename_audio_files(folder_path, role_name, output_dir, log_prefix):
+    # 创建带有时间戳的唯一输出文件名
+    current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_filename = f"{log_prefix}_文件对照表_{current_datetime}.txt"
+    output_file = os.path.join(output_dir, log_filename)
 
-a = 1
-current_dir = os.path.dirname(__file__)
-raw_audio_path = os.path.join(current_dir, "raw_audio")
-print("请输入说话人名，请注意只支持纯字母数字组合")
-name = input()
+    renamed_files = {}
+    total_files = 0
 
-# 获取目标文件夹下所有.wav文件
-wav_files = [file for file in os.listdir(
-    raw_audio_path) if file.endswith('.wav')]
+    # 统计音频文件的总数
+    for root, dirs, files in os.walk(folder_path):
+        total_files += len([filename for filename in files if filename.endswith('.wav')])
 
-a = 1
+    current_index = 0
 
-for wav_file in wav_files:
-    new_file_name = f'{name}_{a}.wav'
-    src_path = os.path.join(current_dir, wav_file)
-    dest_path = os.path.join(current_dir, new_file_name)
-    os.rename(src_path, dest_path)
-    print(f'{src_path} -> {dest_path}')
-    with open("文件对照表.txt", "w") as f:
-        f.write(f"{src_path}->{dest_path}\n")
-    a += 1
+    # 创建并更新进度条
+    def update_progress_bar():
+        nonlocal current_index
+        current_index += 1
+        progress = (current_index / total_files) * 100
+        sys.stdout.write(f'\r[{int(progress)}%] {"#" * int(progress)}{" " * (100 - int(progress))}')
+        sys.stdout.flush()
 
-print("重命名完成")
+    # 遍历文件夹并重命名音频文件
+    for root, dirs, files in os.walk(folder_path):
+        for filename in files:
+            if filename.endswith('.wav'):
+                current_index += 1
+                update_progress_bar()
+                new_filename = f"{role_name}_{current_index}.wav"
+                while os.path.exists(os.path.join(root, new_filename)):
+                    current_index += 1
+                    new_filename = f"{role_name}_{current_index}.wav"
+                new_filepath = os.path.join(root, new_filename)
+                os.rename(os.path.join(root, filename), new_filepath)
+                renamed_files[filename] = new_filename
+
+    # 将重命名日志写入文本文件
+    with open(output_file, 'w') as log_file:
+        for old_name, new_name in renamed_files.items():
+            log_file.write(f"{old_name} -> {new_name}\n")
+
+    print("\n重命名完成。\n重命名日志为:", output_file)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="重命名音频文件")
+    parser.add_argument("-n", "--name", required=True, help="角色名前缀")
+    parser.add_argument("-o", "--output", default="./", help="输出的文件夹")
+    parser.add_argument("-p", "--prefix", default="文件对照表", help="日志文件前缀")
+    args = parser.parse_args()
+
+    folder_path = os.path.join(os.getcwd(), 'raw_audio')  # 根据需要修改文件夹路径
+    rename_audio_files(folder_path, args.name, args.output, args.prefix)
